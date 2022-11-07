fae72cee4eeeeffa78c5dd68d44af81062abb9f2
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-11-07 15:56:04 +0600

    delete user info done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,7 +25,13 @@ const run = async () => {
     try {
         // ---> database collections
         const usersCollection = client.db("nodeMongoCRUD").collection("users");
-
+        // ---> read user 
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const cursor = usersCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
 
         // ---> create user || insert user data
         app.post('/users', async (req, res) => {
@@ -34,13 +40,15 @@ const run = async () => {
             res.send(result)
         })
 
-        // ---> read user 
-        app.get('/users', async (req, res) => {
-            const query = {};
-            const cursor = usersCollection.find(query);
-            const result = await cursor.toArray();
+        // ---> delete user data
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
             res.send(result)
         })
+
+
 
 
     } finally { }
