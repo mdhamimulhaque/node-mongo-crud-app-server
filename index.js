8814cb006968eaff16c89ef46059522379f9cd6c
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-11-07 12:18:04 +0600

    basic setup

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// ---> middle wares
+app.use(cors());
+app.use(express.json());
+
+
+// ---> app testing
+app.get('/', (req, res) => {
+    res.send("node mongo crud app server running...")
+})
+
+// ---> mongodb setup
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASS}@cluster0.76zc9vk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// ---> use bd
+const run = async () => {
+    try {
+
+
+    } finally { }
+
+}
+run().catch(err => console.log(err))
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`running port is ${port}`)
+})
